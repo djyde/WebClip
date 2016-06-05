92a1375217289921c2891a6316e0533e846dea1d
djyde <randypriv@gmail.com>
2016-06-05 18:15:13 +0800

    improve structure

--- build/webpack.config.cmd.js
+++ build/webpack.config.cmd.js
@@ -1,7 +1,7 @@
 const config = require('./webpack.config.base')
 
 config.output.libraryTarget = 'commonjs'
-config.output.filename = '[name].js'
-config.output.path = './lib'
+config.output.filename = '[name].common.js'
+config.output.path = './dist'
 
 module.exports = config
--- build/webpack.config.umd.js
+++ build/webpack.config.umd.js
@@ -1,7 +1,7 @@
 const config = require('./webpack.config.base')
 
 config.output.libraryTarget = 'umd'
-config.output.filename = '[name].js'
+config.output.filename = '[name].min.js'
 config.output.path = './dist'
 
 module.exports = config
